ede23c146fc6bde9d55a59730671dd2a45fc299c
walli546 <walli5446@gmail.com>
2019-10-17 18:23:04 +0200

    feat(triggers): Serialization of Actions

---
+++ src/serialization/SetStateValueActionSerializer.ts
@@ -0,0 +1,53 @@
+import { ActionSerializer } from './ActionSerializer';
+import { SetStateValueAction } from '../actions/SetStateValueAction';
+import { StateService } from '../services/StateService';
+
+export class SetStateValueActionSerializer extends ActionSerializer {
+	public static readonly TYPE: string = 'setStateValueAction';
+
+	constructor(private readonly stateService: StateService) {
+		super();
+	}
+
+	deserialize(stringToDeserialize: string): SetStateValueAction<string | number> {
+		const json = JSON.parse(stringToDeserialize);
+		if (json.type !== SetStateValueActionSerializer.TYPE) {
+			throw new Error(`Type must be ${SetStateValueActionSerializer.TYPE}.`);
+		}
+		if (!json.valueType) {
+			throw new Error('Must contain property valueType');
+		}
+		const baseAction = super.deserialize(stringToDeserialize);
+		if (json.valueType == 'string') {
+			return new SetStateValueAction<string>(
+				baseAction.getId(),
+				baseAction.getTrigger(),
+				json.idOfStateToSet,
+				json.valueToSet,
+				this.stateService,
+			);
+		} else if (json.valueType == 'number') {
+			return new SetStateValueAction<number>(
+				json.id,
+				baseAction.getTrigger(),
+				json.idOfStateToSet,
+				json.valueToSet,
+				this.stateService,
+			);
+		} else {
+			throw new Error(`valueType ${json.valueType} can not be deserialized.`);
+		}
+	}
+
+	serialize(objectToSerialize: SetStateValueAction<string | number>): string {
+		const baseAction = JSON.parse(super.serialize(objectToSerialize));
+		const valueType = typeof objectToSerialize.getValueToSet();
+		return JSON.stringify({
+			...baseAction,
+			idOfStateToSet: objectToSerialize.getIdOfStateToSet(),
+			valueToSet: objectToSerialize.getValueToSet(),
+			valueType: valueType,
+			type: SetStateValueActionSerializer.TYPE,
+		});
+	}
+}
