0a2b56ed4723f5008d5f4700266e6eef8cc24617
walli545 <walli5446@googlemail.com>
2020-04-10 22:54:07 +0200

    Refactor serialization

---
+++ src/serialization/OnOffStateActionSerializer.ts
@@ -0,0 +1,56 @@
+import { Serializer } from './Serializer';
+import { Action } from '../actions/Action';
+import { StateService } from '../services/StateService';
+import { OnOffStateActionBuilder } from '../actions/OnOffStateActionBuilder';
+import { OnOffStateAction } from '../actions/OnOffStateAction';
+
+export class OnOffStateActionSerializer implements Serializer<Action> {
+	private readonly builder: OnOffStateActionBuilder<string | number | boolean>;
+
+	constructor(stateService: StateService) {
+		this.builder = new OnOffStateActionBuilder<string | number | boolean>();
+		this.builder.setStateService(stateService);
+	}
+
+	deserialize(stringToDeserialize: string): Action {
+		const json = JSON.parse(stringToDeserialize);
+		if (json.type !== this.getType()) {
+			throw new Error(`Can not deserialize object of type ${json.type}`);
+		}
+		if (!this.hasCorrectValueType(json)) {
+			throw new Error(`Can not deserialize OnOffStateAction with value type ${json.valueType}`);
+		}
+		return this.builder
+			.setOffValue(json.offValue)
+			.setOnValue(json.onValue)
+			.setBooleanValue(json.booleanValue)
+			.setIdOfStateToSet(json.idOfStateToSet)
+			.build();
+	}
+
+	serialize(objectToSerialize: Action): string {
+		if (objectToSerialize == null) {
+			throw new Error('objectToSerialize may not be null or undefined.');
+		}
+		if (objectToSerialize instanceof OnOffStateAction) {
+			return JSON.stringify({
+				type: this.getType(),
+				valueType: typeof objectToSerialize.getOnValue(),
+				onValue: objectToSerialize.getOnValue(),
+				offValue: objectToSerialize.getOffValue(),
+				booleanValue: objectToSerialize.getBooleanValue(),
+				idOfStateToSet: objectToSerialize.getIdOfStateToSet(),
+			});
+		} else {
+			throw new Error('objectToSerialize must be of type OnOffStateAction.');
+		}
+	}
+
+	public getType(): string {
+		return OnOffStateAction.prototype.constructor.name;
+	}
+
+	private hasCorrectValueType(json: any): boolean {
+		return ['string', 'number', 'boolean'].indexOf(json.valueType) != -1;
+	}
+}
--- test/ts/serializers/TimeTriggerSerializer.test.ts
+++ test/ts/serializers/TimeTriggerSerializer.test.ts
@@ -1,81 +1,233 @@
+import * as TypeMoq from 'typemoq';
 import { TimeTriggerSerializer } from '../../../src/serialization/TimeTriggerSerializer';
 import { TimeTrigger } from '../../../src/triggers/TimeTrigger';
 import { Weekday } from '../../../src/triggers/Weekday';
 import { expect } from 'chai';
+import { StateService } from '../../../src/services/StateService';
+import { OnOffStateActionSerializer } from '../../../src/serialization/OnOffStateActionSerializer';
+import { TimeTriggerBuilder } from '../../../src/triggers/TimeTriggerBuilder';
+import { OnOffStateActionBuilder } from '../../../src/actions/OnOffStateActionBuilder';
+import { Trigger } from '../../../src/triggers/Trigger';
+import { fail } from 'assert';
+import { OnOffStateAction } from '../../../src/actions/OnOffStateAction';
 
 describe('TimeTriggerSerializer', () => {
+	const stateService = TypeMoq.Mock.ofType<StateService>();
+	const onOffStateActionSerializer = new OnOffStateActionSerializer(stateService.object);
+	const onOffStateAction = new OnOffStateActionBuilder()
+		.setOnValue('ON')
+		.setOffValue('OFF')
+		.setBooleanValue(true)
+		.setIdOfStateToSet('id.of.state')
+		.setStateService(stateService.object)
+		.build();
+
 	describe('serialize', () => {
-		it('should serialize ', () => {
-			const sut = new TimeTriggerSerializer();
-			const trigger = new TimeTrigger(12, 30, [Weekday.Monday, Weekday.Thursday]);
+		it('should serialize with OnOffStateAction', () => {
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const trigger = new TimeTriggerBuilder()
+				.setHour(12)
+				.setMinute(30)
+				.setWeekdays([Weekday.Monday, Weekday.Thursday])
+				.setAction(onOffStateAction)
+				.build();
 			const serialized = sut.serialize(trigger);
 			const json = JSON.parse(serialized);
-			expect(json.type).to.equal(TimeTriggerSerializer.TYPE);
+			expect(json.type).to.equal(TimeTrigger.prototype.constructor.name);
 			expect(json.hour).to.equal(12);
 			expect(json.minute).to.equal(30);
 			expect(json.weekdays.length).to.equal(2);
 			expect(json.weekdays.includes(Weekday.Monday)).to.equal(true);
 			expect(json.weekdays.includes(Weekday.Thursday)).to.equal(true);
+			expect(json.action).to.deep.equal(JSON.parse(onOffStateActionSerializer.serialize(onOffStateAction)));
 		});
 
 		it('throws when objectToSerialize is null', () => {
-			const sut = new TimeTriggerSerializer();
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
 			expect(() => sut.serialize(null as any)).to.throw();
+		});
+
+		it('throws when objectToSerialize is undefined', () => {
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			expect(() => sut.serialize(undefined as any)).to.throw();
+		});
+
+		it('throws when objectToSerialize is no instance of TimeTrigger', () => {
+			const anotherTrigger = {
+				getId: () => '0',
+				getAction: () => onOffStateAction, // eslint-disable-next-line @typescript-eslint/no-empty-function
+				trigger: () => {},
+				getWeekdays: () => [Weekday.Monday],
+			} as Trigger;
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			expect(() => sut.serialize(anotherTrigger)).to.throw();
+		});
+
+		it('throws when no serializer for action was found', () => {
+			const sut = new TimeTriggerSerializer([]);
+			const trigger = new TimeTriggerBuilder()
+				.setHour(12)
+				.setMinute(30)
+				.setWeekdays([Weekday.Monday, Weekday.Thursday])
+				.setAction(onOffStateAction)
+				.build();
+			expect(() => sut.serialize(trigger)).to.throw();
 		});
 	});
 
 	describe('deserialize', () => {
-		it('should deserialize ', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = `{"type": "${TimeTriggerSerializer.TYPE}", "hour": 5, "minute": 57, "weekdays": [3, 5, 4]}`;
+		it('should deserialize with OnOffStateAction', () => {
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"id": "0",
+				"hour": 5,
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
 			const deserialized = sut.deserialize(serialized);
-			expect(deserialized.getHour()).to.equal(5);
-			expect(deserialized.getMinute()).to.equal(57);
-			expect(deserialized.getWeekdays().length).to.equal(3);
-			expect(deserialized.getWeekdays().includes(Weekday.Thursday)).to.equal(true);
-			expect(deserialized.getWeekdays().includes(Weekday.Wednesday)).to.equal(true);
-			expect(deserialized.getWeekdays().includes(Weekday.Friday)).to.equal(true);
+			if (deserialized instanceof TimeTrigger) {
+				expect(deserialized.getId()).to.equal('0');
+				expect(deserialized.getHour()).to.equal(5);
+				expect(deserialized.getMinute()).to.equal(57);
+				expect(deserialized.getWeekdays().length).to.equal(3);
+				expect(deserialized.getWeekdays().includes(Weekday.Thursday)).to.equal(true);
+				expect(deserialized.getWeekdays().includes(Weekday.Wednesday)).to.equal(true);
+				expect(deserialized.getWeekdays().includes(Weekday.Friday)).to.equal(true);
+			} else {
+				fail(`Deserialization produced wrong trigger type`);
+			}
+			const action = deserialized.getAction();
+			if (action instanceof OnOffStateAction) {
+				expect(action.getOffValue()).to.equal('OFF');
+				expect(action.getOnValue()).to.equal('ON');
+				expect(action.getBooleanValue()).to.be.true;
+			} else {
+				fail(`Deserialization produced wrong action type`);
+			}
 		});
 
 		it('throws when type is not time', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = `{"type": "astro", "hour": 5, "minute": 57, "weekdays": [3, 5, 4]}`;
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "somewrongtype",
+				"id": "0",
+				"hour": 5,
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
 			expect(() => sut.deserialize(serialized)).to.throw();
 		});
 
 		it('throws when stringToDeserialize is empty', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = '';
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = ``;
 			expect(() => sut.deserialize(serialized)).to.throw();
 		});
 
 		it('throws when stringToDeserialize is invalid json', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = 'abc';
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `abc`;
 			expect(() => sut.deserialize(serialized)).to.throw();
 		});
 
 		it('throws when property type is missing', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = `{"minute": 12, "hour": 10, "weekdays": [1]}`;
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"id": "0",
+				"hour": 5,
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
 			expect(() => sut.deserialize(serialized)).to.throw();
 		});
 
 		it('throws when property hour is missing', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = `{"type": "${TimeTriggerSerializer.TYPE}", "minute": 57, "weekdays": [3, 5, 4]}`;
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"id": "0",
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
 			expect(() => sut.deserialize(serialized)).to.throw();
 		});
 
 		it('throws when property minute is missing', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = `{"type": "${TimeTriggerSerializer.TYPE}", "hour": 10, weekdays": [3, 5, 4]}`;
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"id": "0",
+				"hour": 5,
+				"weekdays": [3, 5, 4],
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
 			expect(() => sut.deserialize(serialized)).to.throw();
 		});
 
 		it('throws when property weekdays is missing', () => {
-			const sut = new TimeTriggerSerializer();
-			const serialized = `{"type": "${TimeTriggerSerializer.TYPE}", "minute": 12, "hour": 10}`;
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"id": "0",
+				"hour": 5,
+				"minute": 57,
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
+			expect(() => sut.deserialize(serialized)).to.throw();
+		});
+
+		it('throws when property id is missing', () => {
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"hour": 5,
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
+			expect(() => sut.deserialize(serialized)).to.throw();
+		});
+
+		it('throws when property action is missing', () => {
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"id": "0",
+				"hour": 5,
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+			}`;
+			expect(() => sut.deserialize(serialized)).to.throw();
+		});
+
+		it('throws when action could not be deserialized', () => {
+			const sut = new TimeTriggerSerializer([onOffStateActionSerializer]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"id": "0",
+				"hour": 5,
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+				"action": {"abc": 3}
+			}`;
+			expect(() => sut.deserialize(serialized)).to.throw();
+		});
+
+		it('throws when no serializer for action was found', () => {
+			const sut = new TimeTriggerSerializer([]);
+			const serialized = `{
+				"type": "${sut.getType()}",
+				"id": "0",
+				"hour": 5,
+				"minute": 57,
+				"weekdays": [3, 5, 4],
+				"action": ${onOffStateActionSerializer.serialize(onOffStateAction)}
+			}`;
 			expect(() => sut.deserialize(serialized)).to.throw();
 		});
 	});
