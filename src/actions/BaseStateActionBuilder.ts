8cc6e668021b41299da9890443b3d07c44d169c5
walli545 <walli5446@googlemail.com>
2020-04-26 14:33:41 +0200

    BaseStateActionBuilder implements Builder

--- src/actions/BaseStateActionBuilder.ts
+++ src/actions/BaseStateActionBuilder.ts
@@ -1,7 +1,8 @@
 import { StateService } from '../services/StateService';
 import { BaseStateAction } from './BaseStateAction';
+import { Builder } from '../Builder';
 
-export abstract class BaseStateActionBuilder {
+export abstract class BaseStateActionBuilder implements Builder<BaseStateAction> {
 	protected stateService: StateService | null = null;
 
 	public setStateService(stateService: StateService): BaseStateActionBuilder {
