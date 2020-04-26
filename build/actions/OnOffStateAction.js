fe638f7014200aac8a532e42f8ad24db00ece83b
walli545 <walli5446@googlemail.com>
2020-04-26 15:14:35 +0200

    Add build files

--- build/actions/OnOffStateAction.js
+++ build/actions/OnOffStateAction.js
@@ -29,29 +29,11 @@ class OnOffStateAction extends BaseStateAction_1.BaseStateAction {
     getOnValue() {
         return this.onValue;
     }
-    setOnValue(onValue) {
-        if (onValue == undefined) {
-            throw new Error('OnValue may not be undefined.');
-        }
-        this.onValue = onValue;
-    }
     getOffValue() {
         return this.offValue;
     }
-    setOffValue(offValue) {
-        if (offValue == undefined) {
-            throw new Error('OffValue may not be undefined.');
-        }
-        this.offValue = offValue;
-    }
     getBooleanValue() {
         return this.booleanValue;
-    }
-    setBooleanValue(booleanValue) {
-        if (booleanValue == null) {
-            throw new Error('ValueToSet may not be null or undefined.');
-        }
-        this.booleanValue = booleanValue;
     }
     execute() {
         const valueToUse = this.getBooleanValue() ? this.getOnValue() : this.getOffValue();
