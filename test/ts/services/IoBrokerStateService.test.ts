e518e9e110645eb827bbfc5374d4f668b07e745f
walli545 <walli5446@googlemail.com>
2020-07-10 15:52:52 +0200

    Add getForeignState() to StateService

--- test/ts/services/IoBrokerStateService.test.ts
+++ test/ts/services/IoBrokerStateService.test.ts
@@ -33,19 +33,19 @@ describe('IoBrokerStateService', () => {
 		it('sets state in adapter', () => {
 			const sut = new IoBrokerStateService(adapterMock.object);
 			sut.setState('test.id.123', 'new value');
-			adapterMock.verify(x => x.setState('test.id.123', 'new value', true), TypeMoq.Times.once());
+			adapterMock.verify((x) => x.setState('test.id.123', 'new value', true), TypeMoq.Times.once());
 		});
 
 		it('sets state in adapter with ack=true', () => {
 			const sut = new IoBrokerStateService(adapterMock.object);
 			sut.setState('test.id.123', 'new value', true);
-			adapterMock.verify(x => x.setState('test.id.123', 'new value', true), TypeMoq.Times.once());
+			adapterMock.verify((x) => x.setState('test.id.123', 'new value', true), TypeMoq.Times.once());
 		});
 
 		it('sets state in adapter with ack=false', () => {
 			const sut = new IoBrokerStateService(adapterMock.object);
 			sut.setState('test.id.123', 'new value', false);
-			adapterMock.verify(x => x.setState('test.id.123', 'new value', false), TypeMoq.Times.once());
+			adapterMock.verify((x) => x.setState('test.id.123', 'new value', false), TypeMoq.Times.once());
 		});
 	});
 
@@ -62,21 +62,84 @@ describe('IoBrokerStateService', () => {
 
 		it('sets state in adapter', () => {
 			sut.setForeignState('test.id.123', 'new value');
-			adapterMock.verify(x => x.setForeignState('test.id.123', 'new value', false), TypeMoq.Times.once());
+			adapterMock.verify((x) => x.setForeignState('test.id.123', 'new value', false), TypeMoq.Times.once());
 		});
 
 		it('should log when logger is provided', () => {
 			const logger = TypeMoq.Mock.ofType<LoggingService>();
 			const sut = new IoBrokerStateService(adapterMock.object, logger.object);
 			sut.setForeignState('test.id.123', 'new value');
-			logger.verify(l => l.logDebug(It.isAnyString()), Times.once());
+			logger.verify((l) => l.logDebug(It.isAnyString()), Times.once());
 		});
 
 		it('should log when value is null', () => {
 			const logger = TypeMoq.Mock.ofType<LoggingService>();
 			const sut = new IoBrokerStateService(adapterMock.object, logger.object);
 			sut.setForeignState('test.id.123', null as any);
-			logger.verify(l => l.logDebug(It.isAnyString()), Times.once());
+			logger.verify((l) => l.logDebug(It.isAnyString()), Times.once());
+		});
+	});
+
+	describe('getForeignState', () => {
+		it('rejects when id is undefined', () => {
+			expect(sut.getForeignState(undefined as any)).to.be.rejectedWith('id may not be null or empty.');
+		});
+
+		it('rejects when id is null', () => {
+			expect(sut.getForeignState(null as any)).to.be.rejectedWith('id may not be null or empty.');
+		});
+
+		it('rejects when id is empty', () => {
+			expect(sut.getForeignState('')).to.be.rejectedWith('id may not be null or empty.');
+		});
+
+		it('rejects when state from callback is null', () => {
+			adapterMock
+				.setup((a) => a.getForeignState('id.of.state', It.isAny()))
+				.returns((_, cb) => {
+					cb(null, null);
+				});
+			expect(sut.getForeignState('id.of.state')).to.be.rejectedWith(
+				'Requested state id.of.state returned null/undefined!',
+			);
+		});
+
+		it('rejects when state from callback is undefined', () => {
+			adapterMock
+				.setup((a) => a.getForeignState('id.of.state', It.isAny()))
+				.returns((_, cb) => {
+					cb(null, undefined);
+				});
+			expect(sut.getForeignState('id.of.state')).to.be.rejectedWith(
+				'Requested state id.of.state returned null/undefined!',
+			);
+		});
+
+		it('rejects when error from callback is set', () => {
+			adapterMock
+				.setup((a) => a.getForeignState('id.of.state', It.isAny()))
+				.returns((_, cb) => {
+					cb('An error occurred', null);
+				});
+			expect(sut.getForeignState('id.of.state')).to.be.rejectedWith('An error occurred');
+		});
+
+		it('rejects when error from callback is set and state is set', () => {
+			adapterMock
+				.setup((a) => a.getForeignState('id.of.state', It.isAny()))
+				.returns((_, cb) => {
+					cb('An error occurred', 'state');
+				});
+			expect(sut.getForeignState('id.of.state')).to.be.rejectedWith('An error occurred');
+		});
+
+		it('resolves with state val', async () => {
+			adapterMock
+				.setup((a) => a.getForeignState('id.of.state', It.isAny()))
+				.returns((_, cb) => {
+					cb(null, { val: 'val1' });
+				});
+			expect(await sut.getForeignState('id.of.state')).to.equal('val1');
 		});
 	});
 });
