868347ebc0f98a3465e305a592a6615260c29377
walli545 <walli5446@googlemail.com>
2020-11-27 23:05:28 +0100

    Change version to 2.2.0-pre.2

--- widgets/time-switch/js/time-switch.js
+++ widgets/time-switch/js/time-switch.js
@@ -1,7 +1,7 @@
 /*
 	ioBroker.vis time-switch Widget-Set
 
-	version: "2.2.0-pre.1"
+	version: "2.2.0-pre.2"
 
 	Copyright 2019 walli545 walli5446@gmail.com
 */
@@ -20,7 +20,7 @@ $.get('../time-switch.admin/words.js', function(script) {
 
 // export vis binds for widget
 vis.binds['time-switch'] = {
-	version: '2.2.0-pre.1',
+	version: '2.2.0-pre.2',
 	showVersion: showVersion,
 	createOnOffWidget: createOnOffWidget,
 	onOffScheduleWidgets: {},
